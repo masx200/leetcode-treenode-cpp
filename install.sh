2c12e374c6a053a979da1d58952a919cb0e22937
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-09 09:08:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
+cd ~
+
 sudo apt install -y libcppunit-dev
+
+cd ~
+git clone https://gitcode.net/mirrors/microsoft/vcpkg
+cd vcpkg
+ sh bootstrap-vcpkg.sh
+ 
+ 
 #git clone https://github.com/google/googletest
 #cd googletest
 
