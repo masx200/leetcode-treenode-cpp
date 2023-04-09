29f3d0a04fad2db56dfe7374ffb6bd8c0c002fcf
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-09 18:15:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,9 @@
 cd ~
-
 sudo apt install -y libcppunit-dev
-
 cd ~
 git clone https://gitcode.net/mirrors/microsoft/vcpkg
 cd vcpkg
 sh bootstrap-vcpkg.sh
- 
- 
 ~/vcpkg/vcpkg integrate install
 ~/vcpkg/vcpkg integrate bash
 ~/vcpkg/vcpkg install  cppunit
- 
- 
