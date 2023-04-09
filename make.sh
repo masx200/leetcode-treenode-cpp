d5e7cf54d2ca5caa88ed0d1419bfd0b816f916ec
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-09 18:44:13 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
-g++ ./*.cxx -fmodules-ts -v -c  -l cppunit  -I ./ 
-g++ ./*.cxx  ./test.cpp -fmodules-ts -v   -l cppunit -o ./test.bin -I ./ 
+g++ ./*.cxx -fmodules-ts -v -c  -l cppunit  -I ./
+
+g++ ./*.cxx  ./test.cpp -fmodules-ts -v   -l cppunit -o ./test.bin -I ./
