9c67a03c4b02e296af0e847a5067a5a96a47e551
masx200 <masx200@qq.com>
2023-04-10 07:56:46 +0800

    fixbug

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-g++ ./*.cxx ./test.cpp -v -fmodules-ts  -c  -l cppunit  -I ./
-g++ ./*.cxx ./test.cpp -v -fmodules-ts  -l cppunit -o ./test.bin -I ./
+g++ ./*.cxx ./test.cpp -v -fmodules-ts  -c  -l cppunit  -I "./"
+g++ ./*.cxx ./test.cpp -v -fmodules-ts  -l cppunit -o ./test.bin -I "./"
