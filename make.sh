7aea7384323b8069ee268e5cf58c375fd71dcdd5
masx200 <masx200@qq.com>
2023-04-12 10:29:08 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,2 +1,4 @@
-g++ ./*.ixx ./*.cpp -std=c++20 -v -fmodules-ts  -c  -l cppunit  -I "./"
-g++ ./*.ixx ./*.cpp  -std=c++20 -v -fmodules-ts  -l cppunit -o ./test.bin -I "./"
+#g++ ./*.ixx ./*.cpp -std=c++20 -v -fmodules-ts  -c  -l cppunit  -I "./"
+#g++ ./*.ixx ./*.cpp  -std=c++20 -v -fmodules-ts  -l cppunit -o ./test.bin -I "./"
+xmake f --toolchain=clang
+xmake build -v
