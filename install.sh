e82c6ba261947058023a923c51208e2f50cb79f9
masx200 <34191203+masx200@users.noreply.github.com>
2023-04-12 09:35:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ sh bootstrap-vcpkg.sh
 ~/vcpkg/vcpkg integrate install
 ~/vcpkg/vcpkg integrate bash
 ~/vcpkg/vcpkg install  cppunit
+
+sudo add-apt-repository ppa:xmake-io/xmake
+sudo apt update
+sudo apt install xmake
+
