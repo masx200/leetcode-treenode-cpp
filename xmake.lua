678cdeef2c8e22819432e8f7b4c78835443e9fcb
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-14 13:59:33 +0800

    debug

--- xmake.lua
+++ xmake.lua
@@ -5,13 +5,25 @@ add_requires("vcpkg::cppunit")
 set_languages("c17", "cxx20")
 set_project("leetcode-treenode-cpp")
 target("leetcode-treenode-cpp")
+-- 如果当前编译模式是debug
+if is_mode("debug") then
 
+    -- 添加DEBUG编译宏
+    add_defines("_DEBUG")
+
+end
 set_kind("static")
 
 add_files("*.ixx", {install = true})
 
 target_end()
 target("test")
+if is_mode("debug") then
+
+    -- 添加DEBUG编译宏
+    add_defines("_DEBUG")
+
+end
 set_group("test")
 set_default(false)
 set_kind("binary")
