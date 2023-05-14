e90a000aa94c0b7c0c26c7372dbfa8cd575f30b9
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-14 14:25:44 +0800

    test

--- xmake.lua
+++ xmake.lua
@@ -1,15 +1,15 @@
 set_policy("platform.longpaths", true)
 set_version("1.1.4")
-add_rules("mode.debug", "mode.release")
+add_rules("mode.test", "mode.release")
 add_requires("vcpkg::cppunit")
 set_languages("c17", "cxx20")
 set_project("leetcode-treenode-cpp")
 target("leetcode-treenode-cpp")
--- 如果当前编译模式是debug
-if is_mode("debug") then
+-- 如果当前编译模式是test
+if is_mode("test") then
 
-    -- 添加DEBUG编译宏
-    add_defines("_DEBUG")
+    -- 添加test编译宏
+    add_defines("__TEST__")
 
 end
 set_kind("static")
@@ -18,10 +18,10 @@ add_files("*.ixx", {install = true})
 
 target_end()
 target("test")
-if is_mode("debug") then
+if is_mode("test") then
 
-    -- 添加DEBUG编译宏
-    add_defines("_DEBUG")
+    -- 添加test编译宏
+    add_defines("__TEST__")
 
 end
 set_group("test")
